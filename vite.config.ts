d613ef3390020e2b319580ad8ce1dad8bf778218
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-12-17 10:55:00 +0100

    add configuration options JSON now supporting only HTTP(s) setting+

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react-swc'
 import dns from 'dns'
-
 dns.setDefaultResultOrder('verbatim')
+
+import { appConfig } from './src/config.js';
+
+const plugins= [react()] 
+if(appConfig.useHTTPs)
+    plugins.push(mkcert())
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), mkcert()],
+    plugins: plugins,
     define: {
         'process.env': process.env
     },   
