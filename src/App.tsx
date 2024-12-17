81c1300f6d3949e9efaf05aa158c48fce97a2f21
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-12-17 18:07:42 +0100

    add onload message with some warnings about cross domain requests

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,56 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Box, ThemeProvider } from "@mui/material";
+import { Box, Slide, Snackbar, ThemeProvider, Alert } from "@mui/material";
 // Custom component libraries
 import { theme } from "./overall-theme";
 import { ThingClient } from './builtins/client/view';
+import React from "react";
 
 
 
 const App = () => {
-    
+
     return (
         <Box id='main-layout' sx={{ display : 'flex', flexGrow : 1, alignItems : 'center'}}>
             <ThemeProvider theme={theme}>      
                 <ThingClient />
+                <OnLoadMessage />
             </ThemeProvider>
+            
         </Box>
+    )
+}
+
+
+const OnLoadMessage = () => {
+
+    const [open, setOpen] = React.useState(true);
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    return (
+        <Snackbar
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={(props) => <Slide {...props} direction="up" />}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        >
+            <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
+                This website makes cross domain requests, therefore:
+                <ul>
+                    <li>credentials are not supported</li>
+                    <li>check your server configuration for CORS headers and allow CORS on the browser</li>
+                    <li>please do report security issues at the GitHub Repository</li>
+                </ul>
+                Please use it at your own risk. This website also does not use cookies. 
+            </Alert>
+        </Snackbar>
     )
 }
 
